a4c0ceedeac3fc62e0795104882e716e8b2cbb32
Thhomas1 <thomasromero10@hotmail.com>
2023-01-10 23:31:02 -0300

    GSAP Intro
    Testing GSAP to test the animations

---
+++ app.js
@@ -0,0 +1,5 @@
+
+gsap.fromTo('.text' ,{opacity: 0, y: 100}, {opacity: 1, duration: 1}) // el 100 modificas la opacida de la animacion uwu
+
+
+// gsap.to('.text', {y: 100, opacity: 0, duration: 1})  esto es por si quiero que baje la animacion de las letras
