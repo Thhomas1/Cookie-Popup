0e43124c9426ccf87c8ec6e9ec12591fc1e84938
Thomas Romero <93294587+Thhomas1@users.noreply.github.com>
2023-01-12 10:38:50 -0300

    Finished

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const tl = gsap.timeline({defaults: {duration: 0.75, ease: "circ.out"}})
 
 tl.fromTo('.cookie-container', {scale:0}, {scale: 1, ease: "elastic.out(1, 0.3)", duration: 1.5}, ) // aparece tipo explosion al poner scale 0 a scale 1 // agg pa que quede mejor // 
-tl.fromTo('.cookie', {opacity: 0, x:-50, rotation: '-45deg'}, {opacity: 1, x:0, rotation: '0deg'}, '<' ) // agg la coma para que no tarde el contenido y sea en simultaneo
+tl.fromTo('.cookie', {opacity: 0, x:-50, rotation: '-45deg'}, {opacity: 1, x:0, rotation: '0deg'}, '<50%' ) // agg la coma para que no tarde el contenido y sea en simultaneo
 tl.fromTo('.text', {x:30, opacity:0}, {x: 0, opacity: 1}, '<') // ! El pacman es para que las animaciones sean al mismo tiempo!!!
 
 
 //  * una animacion va despues de la otra 
 
-// !2:00 QUEDAN
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', ()  => {
+    gsap.to('.cookie-container', {opacity: 0, y:100, duration: 0.75, ease: "circ.out"})
+})
+
+
+
+
+
+
+
+
+
 
 // gsap.fromTo('.text' ,{opacity: 0, y: 100}, {opacity: 1, duration: 1}) // el 100 modificas la opacida de la animacion uwu
 
